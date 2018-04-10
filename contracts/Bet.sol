c26d9cafdf6a78b99563ca226e7cd8f1d4bf4211
XatMassacrE <xxat.massacre@gmail.com>
2018-04-10 16:20:22 +0800

    Add some param desc and update bet

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -8,24 +8,28 @@ import './utils/strings.sol';
 
 contract Bet is usingOraclize {
   using strings for *;
+  address public owner;
 
+  event LogDistributeReward(address addr, uint reward);
   event LogGameResult(bytes32 indexed category, bytes32 indexed gameId, string result);
   event LogPlayerChoice(address addr, uint choice);
 
   /** 
-    gameId: is a fixed string just like "0021701030"
-      the full gameId encode(include football, basketball, esports..) will publish on github
-    leftOdds: need divide 100, if odds is 216 means 2.16
-    rightOdds: need divide 100, if odds is 216 means 2.16
-    spread: need add 0.5, if spread is 0 means 0.5
-    */
+   * @desc
+   * gameId: is a fixed string just like "0021701030"
+   *   the full gameId encode(include football, basketball, esports..) will publish on github
+   * leftOdds: need divide 100, if odds is 216 means 2.16
+   * middleOdds: need divide 100, if odds is 175 means 1.75
+   * rightOdds: need divide 100, if odds is 250 means 2.50
+   * spread: need add 0.5, if spread is 0 means 0.5
+   */
   bytes32 public category;
   bytes32 public gameId;
   uint public deposit;
   uint public minimumBet;
   uint public spread;
   uint public leftOdds;
-  uint public drawOdds;
+  uint public middleOdds;
   uint public rightOdds;
 
   struct Player {
@@ -33,49 +37,49 @@ contract Bet is usingOraclize {
     uint choice;
   }
 
+  /**
+   * @desc
+   * winChoice: Indicate the winner choice of this betting
+   *   1 means leftTeam, 2 means rightTeam, 4 means draw(leftTeam is not always equivalent to the home team)
+   * flag: Indicate which team take spread
+   *   1 means leftTeam, 2 means rightTeam
+   * duration: Indicate the time _this game will last
+   */
   uint public totalBetAmount;
+  uint public leftAmount;
+  uint public middleAmount;
+  uint public rightAmount;
   uint public numberOfBet;
   uint public leftPts;
   uint public rightPts;
-  // 1 means leftTeam win, 2 means rightTeam win
   uint public winChoice;
-  uint public maxOdds;
-  // flag indicate which team take spread, 1 means leftTeam, 2 means rightTeam
   uint public flag;
-  // winOdds determine the odds of winners
-  uint public winOdds;
   uint public duration = 3600 * 3;
-  address[] public players;
 
+  address [] players;
   mapping(address => Player) public playerInfo;
 
   function() payable public {}
 
-  function setMaxOdds(uint odds1, uint odds2) internal pure returns (uint) {
-    if (odds1 > odds2) {
-      return odds1;
-    } else {
-      return odds2;
-    }
-  }
-
   function Bet(bytes32 _category, bytes32 _gameId, uint _deposit, uint _minimumBet, 
-                  uint _spread, uint _leftOdds, uint _rightOdds, uint _flag) payable public {
+                  uint _spread, uint _leftOdds, uint _middleOdds, uint _rightOdds, uint _flag) payable public {
+    owner = msg.sender;
+
     flag = _flag;
-    setMaxOdds(leftOdds, rightOdds);
     category = _category;
     gameId = _gameId;
     deposit = _deposit;
     minimumBet = _minimumBet;
     spread = _spread;
     leftOdds = _leftOdds;
+    middleOdds = _middleOdds;
     rightOdds = _rightOdds;
-    
+
     // oraclize_setCustomGasPrice(4000000000 wei);
     // oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
 
     // Set a delay close function
-    close();
+    // close();
   }
 
   function getQueryUrl(bytes32 _gameId) internal pure returns (string) {
@@ -86,32 +90,53 @@ contract Bet is usingOraclize {
     return ''.toSlice().join(parts);
   }
 
-  function close() internal {
+  function close() public {
     if (oraclize_getPrice("URL") > address(this).balance) {
+      refund();
     } else {
       string memory url = getQueryUrl(gameId);
-      oraclize_query(duration, "URL", url);
+      // oraclize_query(duration, "URL", url);
+      oraclize_query(0, "URL", url);
     }
   }
 
   function checkPlayerExists(address player) view public returns (bool) {
-    for(uint i = 0; i < players.length; i++) {
-      if(players[i] == player) return true;
+    if (playerInfo[player].choice > 0) {
+      return true;
     }
     return false;
   }
 
-  function placeBet(uint choice) public payable {
-    require(!checkPlayerExists(msg.sender));
-    require(choice == 1 ||  choice == 2);
-    require(msg.value >= minimumBet);
-    require(maxOdds * (totalBetAmount + msg.value) / 100 > deposit);
+  function isSolvent(uint choice, uint amount) view internal returns (bool) {
+    uint needAmount;
+    if (choice == 1) {
+      needAmount = leftOdds * (leftAmount + amount) / 100;
+    } else if (choice == 2) {
+      needAmount = middleOdds * (middleAmount + amount) / 100;
+    } else {
+      needAmount = rightOdds * (rightAmount + amount) / 100;
+    }
 
-    playerInfo[msg.sender].betAmount = msg.value;
+    if (needAmount > totalBetAmount + amount + deposit) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function placeBet(uint choice) public payable {
+    require(choice == 1 ||  choice == 2 || choice == 3);
+    require(msg.value >= minimumBet);
+
+    if (!isSolvent(choice, msg.value)) {
+      revert();
+    }
+
+    playerInfo[msg.sender].betAmount += msg.value;
     playerInfo[msg.sender].choice = choice;
 
     totalBetAmount += msg.value;
-    players.push(msg.sender);
+    numberOfBet += 1;
     LogPlayerChoice(msg.sender, choice);
   }
 
@@ -140,15 +165,31 @@ contract Bet is usingOraclize {
 
     require(winChoice == 1 || winChoice == 2);
     if (winChoice == 1) {
-      winOdds = leftOdds;
+      distributeReward(leftOdds);
     } else {
-      winOdds = rightOdds;
+      distributeReward(rightOdds);
     }
-
-    distributeReward();
   }
 
-  function distributeReward() internal {
+  function refund() {
+    for(uint i = 0; i < players.length; i++) {
+      address playerAddress = players[i];
+      playerAddress.transfer(playerInfo[playerAddress].betAmount);
+    }
+  }
+
+  function testDistribute(uint winOdds) public {
+    for(uint i = 0; i < players.length; i++) {
+      address playerAddress = players[i];
+      if(playerInfo[playerAddress].choice == winChoice) {
+        // Distribute ether to winners
+        LogDistributeReward(playerAddress, winOdds * playerInfo[playerAddress].betAmount / 100);
+        playerAddress.transfer(winOdds * playerInfo[playerAddress].betAmount / 100);
+      }
+    }
+  }
+
+  function distributeReward(uint winOdds) internal {
     for(uint i = 0; i < players.length; i++) {
       address playerAddress = players[i];
       if(playerInfo[playerAddress].choice == winChoice) {
