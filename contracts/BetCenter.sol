5779a269750506721f43494a3a6d603cedcd3d71
XatMassacrE <xxat.massacre@gmail.com>
2018-04-17 01:23:09 +0800

    Add new parameter to create bet

--- contracts/BetCenter.sol
+++ contracts/BetCenter.sol
@@ -6,16 +6,17 @@ contract BetCenter {
 
   mapping(bytes32 => Bet[]) public bets;
 
-  event LogCreateBet(address dealerAddr, bytes32 category);
+  event LogCreateBet(address indexed dealerAddr, address betAddr, bytes32 category);
 
   function() payable public {}
 
   function createBet(bytes32 category, bytes32 gameId, uint deposit, uint minimumBet, 
-                  uint spread, uint awayOdds, uint homeOdds, uint _flag) payable public {
+                  uint spread, uint leftOdds, uint middleOdds, uint rightOdds, uint _flag,
+                  uint startTime, uint duration) payable public {
     Bet bet = new Bet(category, gameId, deposit, minimumBet, 
-                  spread, awayOdds, homeOdds, _flag);
+                  spread, leftOdds, middleOdds, rightOdds , _flag, startTime, duration);
     bets[category].push(bet);
-    LogCreateBet(msg.sender, category);
+    LogCreateBet(msg.sender, bet, category);
   }
 
   function getBetsByCategory(bytes32 category) constant public returns (Bet[]) {
