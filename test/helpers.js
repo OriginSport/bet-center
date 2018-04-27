bb0e34b20426b92aec179085c9e4a4eb8a241af5
XatMassacrE <xxat.massacre@gmail.com>
2018-04-27 18:18:33 +0800

    Add helpers.js to assist test functions

---
+++ test/helpers.js
@@ -0,0 +1,18 @@
+module.exports = {
+  assertRevert: async (promise) => {
+    try {
+      await promise;
+    } catch (error) {
+      const revertFound = error.message.search('revert') >= 0;
+      assert(revertFound, `Expected "revert", got ${error} instead`);
+      return;
+    }
+    assert.fail('Expected revert not received');
+  },
+  addsDayOnEVM: async function(days) {
+		var daysInsecond = 60 * 60 * 24 * days
+		var currentBlockTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp;
+		await web3.currentProvider.send({jsonrpc: "2.0", method: "evm_increaseTime", params: [daysInsecond], id: 0});
+		await web3.currentProvider.send({jsonrpc: "2.0", method: "evm_mine", params: [], id: 0});
+	}
+}
