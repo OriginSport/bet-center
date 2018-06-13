81c2e51f2070e46139aabbb3e731b44b25cae3ba
XatMassacrE <xxat.massacre@gmail.com>
2018-06-13 22:25:14 +0800

    Add getGameIdsBycategory function

--- contracts/BetCenter.sol
+++ contracts/BetCenter.sol
@@ -6,6 +6,7 @@ import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 contract BetCenter is Ownable {
 
   mapping(bytes32 => Bet[]) public bets;
+  mapping(bytes32 => bytes32[]) public gameIds;
 
   event LogCreateBet(address indexed dealerAddr, address betAddr, bytes32 indexed category, uint indexed startTime);
 
@@ -17,6 +18,7 @@ contract BetCenter is Ownable {
     Bet bet = (new Bet).value(msg.value)(msg.sender, category, gameId, minimumBet, 
                   spread, leftOdds, middleOdds, rightOdds , flag, startTime, confirmations, owner);
     bets[category].push(bet);
+    gameIds[category].push(gameId);
     LogCreateBet(msg.sender, bet, category, startTime);
   }
 
@@ -26,6 +28,10 @@ contract BetCenter is Ownable {
    */
   function getBetsByCategory(bytes32 category) view public returns (Bet[]) {
     return bets[category];
+  }
+
+  function getGameIdsByCategory(bytes32 category) view public returns (bytes32 []) {
+    return gameIds[category];
   }
 
 }
